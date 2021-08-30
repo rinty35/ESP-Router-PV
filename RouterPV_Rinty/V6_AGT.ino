9d26cdae1ea1c646e21c451d0a93ceb5cf656e95
Aurelien Ga <agtemp0@laposte.net>
2021-08-30 19:59:25 +0200

    Correction communication 9600Bauds

--- RouterPV_Rinty/V6_AGT.ino
+++ RouterPV_Rinty/V6_AGT.ino
@@ -19,19 +19,14 @@
 #define ON 1  // commande positive du relai statique ou triac
 #define OFF 0
 #define version "Rinty35"
-#define Fireimediatly 200
+#define Fireimediatly 0
+ 
 
 #define CdeCh1 2// pin4 micro cde SSR1
 #define CdeCh2 4// 8 pin 14 micro cde SSR2 (ou 4 pin 6)
 #define voltageSensorPin A3
 #define currentSensorPin A5
 
-//++++++++ PARAMETRES A MODIFIER SUIVANT INSTALL++++++
-#define CE 1350   // puissance Chauffe eau W 
-#define capacityOfEnergyBucket 3600 // AU LIEU DE 3600 (900 capacité des panneaux 1800KVC à remplir le bucket en 1/2s)
-#define niveaubucketbas 200 //seuil bas pour lequel on ne déclanche pas 
-float safetyMargin_watts = 10;  // <<<------ increase for more export
-//+++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 
@@ -40,6 +35,7 @@ long cptperiodes = 0;
 int  samplesDuringThisMainsCycle = 0;
 byte nextStateOfTriac;
 float cyclesPerSecond = 50; // flottant pour plus de précision
+float halfsin = 1000000 / cyclesPerSecond / 2; //temps pour une 1/2 onde
 byte polarityNow;
 boolean beyondStartUpPhase = false;
 float energyInBucket = 0;                                              
@@ -69,15 +65,27 @@ float  phaseShiftedVminusDC;
 float instP;
 float realPower;
 float realEnergy;
+//++++++++ PARAMETRES A MODIFIER SUIVANT INSTALL++++++
+#define CE 2000   // puissance Chauffe eau W 
+#define capacityOfEnergyBucket 1000 // AU LIEU DE 3600 (900 capacité des panneaux 1800KVC à remplir le bucket en 1/2s)
+float safetyMargin_watts = 30;  // <<<------ increase for more export
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+int voie2 = 0;
+#define minpowerbucketforfire  ((8500/halfsin)-1)*-capacityOfEnergyBucket  //calcul du seuil de bucket pour lequel le code pourra déclancher un tire sans générer de parasite seuil établi à 8500microseconds
+#define powerforfireimediatly  ((0/halfsin)-1)*-capacityOfEnergyBucket
+#define Minpowerroutable CE/cyclesPerSecond *(minpowerbucketforfire/capacityOfEnergyBucket)
 
-#define Minpowerroutable CE/cyclesPerSecond *(100/(float)capacityOfEnergyBucket)
+float sumenergyInBucket = 0;
+float sumrealPower = 0;
+unsigned long sumfiringDelayInMicros = 0;
+
 
 void setup()
 { 
   
    wdt_enable(WDTO_8S);
   
-  Serial.begin(2400); // pour tests
+  Serial.begin(9600); // pour tests
   pinMode(CdeCh1, OUTPUT); 
   pinMode(CdeCh2, OUTPUT); 
   digitalWrite(CdeCh1, OFF); 
@@ -91,7 +99,7 @@ void setup()
   // ++++++++++++++++++++++++++++++++++++++++++++++++++++++
   
     
-  POWERCAL = 0.18 ;   // org 0.12 à ajuster pour faire coincider la puissance vraie avec le realPwer.
+  POWERCAL = 0.18 ;   //tigous : 0.18 org 0.12 à ajuster pour faire coincider la puissance vraie avec le realPwer.
                     // en utilisant le traceur serie.
                     // NON CRITIQUE car les valeur absolues s'annulent en phase de régulation         // retort and export flows are balanced.  
                     
@@ -148,7 +156,7 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 		  
 		// mise à jour du filtre passe bas pour la soustraction de la composante continue
 			prevDCoffset = DCoffset;
-			DCoffset = prevDCoffset + (0.01 * cumVdeltasThisCycle); 
+			DCoffset = prevDCoffset + (0.015 * cumVdeltasThisCycle); 
 
 		//  Calcul la puissance réelle de toutes les mesures echantillonnées durant 
 		//  le cycle précedent, et determination du gain (ou la perte) en energie.
@@ -156,26 +164,10 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 			realEnergy = realPower / cyclesPerSecond;
       cycleCount++; // incrément Nb de périodes  
 
-//envoi des informations sur le port série
-      if((cycleCount >= 150)) // display once per second
-      {
-        String message = "EIB>" + (String)energyInBucket; 
-        message += ";REE>" + (String)realPower; // Power mesured
-        message +=";MPC>" + (String) (Minpowerroutable * cyclesPerSecond); //Min Power Charge
-        message += ";POR>" + (String)(CE*(1-((float)firingDelayInMicros/10000))); //Energy routed
-        message += ";FID>" + (String)(firingDelayInMicros); //Firedelay
-        message += ";SPL>" + (String)(samplesDuringThisMainsCycle);
-        message += ";";
-        message += "$"+ (String)CRC8(message) ;
-        Serial.println(message);
-       /* Serial.print(";MPC>"); Serial.print(Minpowerroutable * cyclesPerSecond); //Min Power Charge
-        Serial.print(";POR>"); Serial.print(CE*(1-((float)firingDelayInMicros/10000))); //Energy routed
-        Serial.print(";FID>"); Serial.print(firingDelayInMicros); //Firedelay
-        Serial.print(";SPL>"); Serial.print(samplesDuringThisMainsCycle);
-        Serial.println(";");
-    */
-        cycleCount = 100;
-      }    
+
+  
+      
+
 		  //----------------------------------------------------------------
 		  // WARNING - Serial statements can interfere with time-critical code, but
 		  //           they can be really useful for calibration trials!
@@ -184,45 +176,52 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 
 
 			if (beyondStartUpPhase == true)// > 2 secondes
-			{  
-		  // Supposant que les filtres ont eu suffisamment de temps de se stabiliser    
-		  // ajout de cette energie d'une période à l'énergie du reservoir.
-/*   min conso EC sur une sinus = puissance du CE * 3600 (pour avoir des Joules) / 3600 (pour avoir Nb Joules par seconde) / 100 (par le double de la fréquence du 220V pour avoir J par demie prédiode)
- *    pourcentage mini d'alimentation = 1-100/capacityOfEnergyBucket
- *    energie min évacuée = pourcentage mini d'alimentation x min conso EC sur une sinus
-      if (energyInBucket < 100 && realEnergy < CE/100*(100/(float)capacityOfEnergyBucket) && realEnergy >0) { 
-      //on est à l'équilibre prod-conso et le sceau n'a que très peu d'énergie. On ne route pas et on remet le sceauà 0
-        energyInBucket = 0;
-      } else {
-        energyInBucket += realEnergy;  
-      }
-*/
-  //    				energyInBucket += realEnergy;   
-      if (energyInBucket < niveaubucketbas && realEnergy < Minpowerroutable && realEnergy >0) { //si le sceau est sous le seuil de routage et que l'énergie mesurée est inférieure au min soutirable par la charge mais positive
-     //on est à l'équilibre prod-conso on remet le sceau à 0
-        energyInBucket = 0;
-      } else if (realEnergy < 0) {      // Reduction d'énergie dans le reservoir d'une quantité "safety margin" afin retablir l'équilibre plus vite
-        energyInBucket += realEnergy;
-        energyInBucket -= safetyMargin_watts / cyclesPerSecond;
-      } else {
-        energyInBucket += realEnergy;
-      }
+			  {  
+		    // Supposant que les filtres ont eu suffisamment de temps de se stabiliser    
+		    // ajout de cette energie d'une période à l'énergie du reservoir.
+        /*   min conso EC sur une sinus = puissance du CE * 3600 (pour avoir des Joules) / 3600 (pour avoir Nb Joules par seconde) / 100 (par le double de la fréquence du 220V pour avoir J par demie prédiode)
+        *    pourcentage mini d'alimentation = 1-100/capacityOfEnergyBucket
+        *    energie min évacuée = pourcentage mini d'alimentation x min conso EC sur une sinus
+          if (energyInBucket < 100 && realEnergy < CE/100*(100/(float)capacityOfEnergyBucket) && realEnergy >0) { 
+          //on est à l'équilibre prod-conso et le sceau n'a que très peu d'énergie. On ne route pas et on remet le sceauà 0
+            energyInBucket = 0;
+          } else {
+            energyInBucket += realEnergy;  
+          }
+        */
+        //if (energyInBucket < minpowerbucketforfire && realEnergy < Minpowerroutable && realEnergy >0) { //si le sceau est sous le seuil de routage et que l'énergie mesurée est inférieure au min soutirable par la charge mais positive on remet le sceau à 0
+        //  energyInBucket = 0;
+        //} else {
+          energyInBucket += realEnergy;  
+        //}
 		 
-
-		  // Ceci permet au système un décalage pour plus d'injection ou + de soutirage 
-	//			energyInBucket -= (safetyMargin_watts / cyclesPerSecond)*(1-energyInBucket/(float)capacityOfEnergyBucket); // Soustraction de la safety margin en fonction du % de remplissage du sceau Plein on ne retire rien, quasi vide on retire la safetymargin
-
-
-		  // Limites dans la fourchette 0 ...1000 joules ;ne peut être négatif le 11 / 2 / 18
+		    // Reduction d'énergie dans le reservoir d'une quantité "safety margin"
+		    // Ceci permet au système un décalage pour plus d'injection ou + de soutirage 
+	      //energyInBucket -= (safetyMargin_watts / cyclesPerSecond)*(0.2-energyInBucket/(float)capacityOfEnergyBucket); // Soustraction de la safety margin en fonction du % de remplissage du sceau Plein on ne retire rien, quasi vide on retire la safetymargin
+        if (energyInBucket<(float)capacityOfEnergyBucket*0.35){
+          energyInBucket -= ((safetyMargin_watts) / cyclesPerSecond);
+        }else {
+          energyInBucket += (safetyMargin_watts) / (cyclesPerSecond);
+        }
+    /*
+        if (energyInBucket<(float)capacityOfEnergyBucket/2){
+          energyInBucket += (((-2*safetyMargin_watts*energyInBucket)/((float)capacityOfEnergyBucket/2-minpowerbucketforfire)+(safetyMargin_watts-minpowerbucketforfire*(-2*safetyMargin_watts)/((float)capacityOfEnergyBucket/2-minpowerbucketforfire))) / cyclesPerSecond);
+        }else {
+          energyInBucket += (safetyMargin_watts / cyclesPerSecond);
+        }
+      */  
+        
+		    // Limites dans la fourchette 0 ...1000 joules ;ne peut être négatif le 11 / 2 / 18
 				if (energyInBucket > capacityOfEnergyBucket)
 					energyInBucket = capacityOfEnergyBucket;  
 				if (energyInBucket < 0)
-					energyInBucket = 0;    
-			}
-			else // Après un reset attendre 100 périodes (2 secondes) le temps que la composante continue soit éliminée par le filtre   
-			{   
-				if(cycleCount > 100) // deux secondes
-					beyondStartUpPhase = true; //croisière
+					energyInBucket = 0; 
+
+ 
+             
+			} else { // Après un reset attendre 100 périodes (2 secondes) le temps que la composante continue soit éliminée par le filtre   
+			  if(cycleCount > 100) // deux secondes
+				  beyondStartUpPhase = true; //croisière
 			}
 
 		  // ********************************************************
@@ -230,31 +229,73 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 		  // determines the correct firing delay for a direct-acting trigger
 
 		  // never fire if energy level is below lower threshold (zero power)  
-			if (energyInBucket <= niveaubucketbas) { firingDelayInMicros = 99999;} 
+			if (energyInBucket < minpowerbucketforfire) { firingDelayInMicros = halfsin;} //remplacement 99999 par 10000 1S /50Hz /2alternances en microssecondes
 			else  
 		  // fire immediately if energy level is above upper threshold (full power)
-			if (energyInBucket >= capacityOfEnergyBucket) { firingDelayInMicros = 0;} 
+			//if (energyInBucket >= capacityOfEnergyBucket) { firingDelayInMicros = 0;} 
+			if (energyInBucket>=powerforfireimediatly){ firingDelayInMicros = Fireimediatly;}// si le calcul du délais est inférieur au délais de démarrage immédiat pour bon armement du triac alors on si positionne sur ce délais
 			else
 		  // determine the appropriate firing point for the bucket's level
 		  // by using either of the following algorithms
 			{ 
-		  // simple algorithm (with non-linear power response across the energy range)
-		  //       firingDelayInMicros = 10 * (capacityOfEnergyBucket - energyInBucket); 
-        firingDelayInMicros = round (10000 * (1-(energyInBucket/capacityOfEnergyBucket))); //(10000 microsecond durée d'un demie cycle * 1- % de charge du bucket)
-      
-		  // complex algorithm which reflects the non-linear nature of phase-angle control.  
-			//	firingDelayInMicros = (asin((-1 * (energyInBucket - (capacityOfEnergyBucket/2)) / 500)) + (PI/2)) * (10000/PI);
+		    // simple algorithm (with non-linear power response across the energy range)
+		    // firingDelayInMicros = 10 * (capacityOfEnergyBucket - energyInBucket); 
+//       if (firingDelayInMicros=halfsin) { // si premier tire d'une série on tire au minimum
+//        firingDelayInMicros = halfsin;
+//       } else {
+        firingDelayInMicros = round (halfsin * (1-(energyInBucket/capacityOfEnergyBucket))); //(10000 microsecond durée d'un demie cycle * (1- % de charge du bucket))
+//       }
+  		  // complex algorithm which reflects the non-linear nature of phase-angle control.  
+			  //	firingDelayInMicros = (asin((-1 * (energyInBucket - 1800) / 500)) + (PI/2)) * (10000/PI);
 			
-			// Suppress firing at low energy levels to avoid complications with 
-			// logic near the end of each half-cycle of the mains.  
-			// This cut-off affects approximately the bottom 5% of the energy range.
-				if (firingDelayInMicros > 8500) { firingDelayInMicros = 99999;} // never fire
-        else if (firingDelayInMicros < Fireimediatly) { firingDelayInMicros = Fireimediatly;} //Déclancment imédiat 500 pour avoir une meilleure tension d'armement du triac 
-			}
+			  // Suppress firing at low energy levels to avoid complications with 
+			  // logic near the end of each half-cycle of the mains.  
+			  // This cut-off affects approximately the bottom 5% of the energy range.
 				
+				
+//				if (firingDelayInMicros > 8500) { firingDelayInMicros = halfsin;} // never fire
+//        else if (firingDelayInMicros < Fireimediatly) { firingDelayInMicros = Fireimediatly;} //Déclancment imédiat Fireimediatly pour avoir une meilleure tension d'armement du triac 
+			}
+      sumenergyInBucket += energyInBucket;
+      sumrealPower += realPower;
+      sumfiringDelayInMicros += firingDelayInMicros;
+      if((cycleCount >= 150)) { // display once per second
+        if (sumenergyInBucket/50 == capacityOfEnergyBucket){
+          if (voie2 == 0) {
+            voie2=20;
+          } else if (voie2<100) {
+            voie2+=1;
+          }
+        } else if (voie2>0 && sumenergyInBucket/50 > capacityOfEnergyBucket*0.9 ) {
+          voie2-=1;
+        }else if (voie2>0 && sumenergyInBucket/50 > capacityOfEnergyBucket*0.5 ) {
+          voie2-=5;
+        }else if (voie2>0 && voie2>10 ) {
+          voie2-=10;
+        }else if (voie2>0 ) {
+          voie2-=1;
+        }
+        String message = "EIB>" + (String)(sumenergyInBucket/50); 
+        message += ";REE>" + (String)(sumrealPower/50); // Power mesured
+        message +=";MPC>" + (String) (Minpowerroutable*cyclesPerSecond); //Min Power Charge en W
+        message += ";POR>" + (String)((1-(sumfiringDelayInMicros/50/halfsin))*100); //Energy routed en %
+        message += ";FID>" + (String)(sumfiringDelayInMicros/50); //Firedelay
+        message += ";SPL>" + (String)(samplesDuringThisMainsCycle);
+        message += ";VO2>" + (String)(voie2);
+        message += ";";
+        message += "$"+ (String)CRC8(message) ;
+        Serial.println(message);
+        cycleCount = 100;
+        sumenergyInBucket=0;
+        sumrealPower=0;
+        sumfiringDelayInMicros=0;
+        //maquette
+        //delay (1000);
+      } 				
 		  // end of section to support phase-angle control of triac
 		  //*******************************************************
-		   
+
+       
 			sumP = 0;
 			samplesDuringThisMainsCycle = 0;
 			cumVdeltasThisCycle = 0;
@@ -295,7 +336,7 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 	{
     // Unless dumping full power, release the trigger on all loops in this 
     // half cycle after the one during which the trigger was set.  
-		if (firingDelayInMicros > Fireimediatly) { // on arrete la demande d'allumage une fois que la sinusoide passe sous le seuil le MOC du SSR s'arrête.    
+		if (firingDelayInMicros > Fireimediatly) {     
 			digitalWrite(CdeCh1, OFF);
 		}
 	}
@@ -321,12 +362,13 @@ void loop() // Une paire tension / courant est mesurée à chaque boucle (enviro
 
   // Apply phase-shift to the voltage waveform to ensure that the system measures a
   // resistive load with a power factor of unity.
-	phaseShiftedVminusDC = 
-                lastSampleVminusDC + PHASECAL * (sampleVminusDC - lastSampleVminusDC);  
+	phaseShiftedVminusDC = lastSampleVminusDC + PHASECAL * (sampleVminusDC - lastSampleVminusDC);  
 	instP = phaseShiftedVminusDC * sampleIminusDC; // PUISSANCE derniers échantillons V x I filtrés 
 	sumP +=instP;     // accumulation à chaque boucle des puissances instantanées dans une période
 
 	cumVdeltasThisCycle += (sampleV - DCoffset); // pour usage avec filtre passe bas
+
+ 
 
 
 } // end of loop()
